652f3885fbaacc9a2a0037d42f1932fb5591b5fa
GepetoinTraining <garcia.pedro.wow@gmail.com>
2025-12-05 18:00:23 -0300

    final try

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001', // For local API testing if needed
+        target: 'http://localhost:3000', // For local API testing if needed
         changeOrigin: true
       }
     }
